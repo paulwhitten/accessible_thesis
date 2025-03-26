dd71797a9c0ee40659626be68d59b6db395d9bdb
pcw <pcw@case.edu>
2025-03-26 17:15:29 -0400

    pcw's updated accessible thesis

---
+++ images/digit-images/index.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0a9cfb298017e937d7fcd1a0e27eba092d77f5f1b7388d5154d4f24c1454c6fb
+size 909
---
+++ images/temp/index.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e3dbdc063c91ad96db4bdd0ae1c41079f804e56b8f577dbf243943729fc7e01
+size 844
